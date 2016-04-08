e2d400c86eab48e26b24e66d45dc70f4d7d1996f
Ilkka Harmanen <ilkka.harmanen@gmail.com>
2016-04-08 16:47:34 +0200

    Added sorting method to Hand class

--- src/main/java/ch/bbv/dojo/Hand.java
+++ src/main/java/ch/bbv/dojo/Hand.java
@@ -13,4 +13,10 @@ public class Hand {
     public static List<Card> hand(String... cardStrings) {
         return Arrays.stream(cardStrings).map(Card::card).collect(Collectors.toList());
     }
+
+    public static List<Card> sort(List<Card> cards) {
+        ArrayList<Card> defCopy = new ArrayList<>(cards);
+        defCopy.sort((o1, o2) -> o1.getValue().compareTo(o2.getValue()));
+        return defCopy;
+    }
 }
