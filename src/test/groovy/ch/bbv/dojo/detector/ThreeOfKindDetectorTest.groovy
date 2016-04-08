e9ac2f3c3683d41ef8813aa1d1b8e2128d3a12e8
Ilkka Harmanen <ilkka.harmanen@gmail.com>
2016-04-08 09:38:48 +0200

    Simple three of Kind test

---
+++ src/test/groovy/ch/bbv/dojo/detector/ThreeOfKindDetectorTest.groovy
@@ -0,0 +1,26 @@
+package ch.bbv.dojo.detector
+
+import spock.lang.Specification
+
+import static ch.bbv.dojo.Hand.hand
+
+/**
+ * Created by gro on 08/04/16.
+ */
+class ThreeOfKindDetectorTest extends Specification {
+    def "Should be three of kind "() {
+        given:
+        def hand = hand("C2", "H2", "D2", "H7", "C13")
+
+        expect:
+        new ThreeOfKindDetector().is(hand)
+    }
+
+    def "Should not be three of kind"() {
+        given:
+        def hand = hand("C12", "H2", "H4", "H7", "C13")
+
+        expect:
+        !new ThreeOfKindDetector().is(hand)
+    }
+}
